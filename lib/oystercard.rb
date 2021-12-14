65b72612808f20e95110854fd637d210ba58a5a0
Ben Coyne <bencoyne123@yahoo.co.uk>
2021-12-14 21:38:49 +0000

    Added testing for entry_station and passing code + edited/deleted tests where necessary

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,31 +3,33 @@ class OysterCard
   MIN_TOUCH_IN_BALANCE = 1
   MAX_BALANCE = 90
 
-  attr_reader :balance, :travelling
+  attr_reader :balance, :travelling, :entry_station
 
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
-    @travelling = false
+    @entry_station = nil
   end
 
   def top_up(amount)
     @amount = amount
     raise "ERROR: MAX-BALANCE IS #{MAX_BALANCE}" if limit?
+
     @balance += amount
   end
 
-  def touch_in
-    raise "ERROR: INSUFFICIENT FUNDS FOR TOUCH_IN" if balance < MIN_TOUCH_IN_BALANCE
-    @travelling = true
+  def touch_in(station)
+    raise 'ERROR: INSUFFICIENT FUNDS FOR TOUCH_IN' if balance < MIN_TOUCH_IN_BALANCE
+
+    @entry_station = station
   end
-  
+
   def touch_out
     deduct(MIN_TOUCH_IN_BALANCE)
-    @travelling = false
+    @entry_station = nil
   end
- 
+
   def in_journey?
-    @travelling
+    !!@entry_station
   end
 
   private
@@ -35,9 +37,8 @@ class OysterCard
   def deduct(amount)
     @balance -= amount
   end
-  
+
   def limit?
     @balance + @amount > MAX_BALANCE
   end
-
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,16 +2,12 @@ require 'oystercard'
 
 describe OysterCard do
   subject(:oystercard) { described_class.new }
-  # In order to use public transport
-  # As a customer
-  # I want money on my card
-  describe "#balance" do 
+  let(:station) { double :station }
 
-    it { is_expected.to respond_to(:balance) }
+  it { is_expected.to respond_to(:balance) }
 
-    it "has a default balance (£DEFAULT_CAPACITY)" do
-      expect(oystercard.balance).to eq(OysterCard::DEFAULT_BALANCE)
-    end
+  it "has a default balance (DEFAULT_CAPACITY)" do
+    expect(oystercard.balance).to eq(OysterCard::DEFAULT_BALANCE)
   end
 
   describe '#top_up' do
@@ -38,46 +34,62 @@ describe OysterCard do
   describe "in_journey?" do
     it { is_expected.to respond_to(:in_journey?) }
 
-    it "returns @travelling" do
-      expect(oystercard.in_journey?).to eq(oystercard.travelling)
-    end
+    # it "returns @travelling" do
+    #   expect(oystercard.in_journey?).to eq(oystercard.entry_station)
+    # end
   end
 
   describe "touch_in" do
-    it { is_expected.to respond_to(:touch_in) }
 
-    it "returns if card is in journey" do
-      oystercard.top_up(OysterCard::MAX_BALANCE)
-      oystercard.touch_in
-      expect(oystercard).to be_in_journey
+    it { is_expected.to respond_to(:touch_in).with(1).argument }
+
+    context "oystercard at max-balance" do
+      before{oystercard.top_up(OysterCard::MAX_BALANCE)} 
+      # In order to pay for my journey
+      # As a customer
+      # I need to know where I've travelled from
+      it "stores the entry station after touch_in" do
+        oystercard.touch_in(station)
+        expect(oystercard.entry_station).to eq station
+      end
+
+      it "returns if card is in journey" do
+        oystercard.touch_in(station)
+        expect(oystercard).to be_in_journey
+      end
     end
-
     # In order to pay for my journey
     # As a customer
     # I need to have the minimum amount (£1) for a single journey.
     it "raises error if balance < 1"  do
       oystercard.touch_out
-      expect { oystercard.touch_in }.to raise_error "ERROR: INSUFFICIENT FUNDS FOR TOUCH_IN"
+      expect { oystercard.touch_in(station) }.to raise_error "ERROR: INSUFFICIENT FUNDS FOR TOUCH_IN"
     end
   end
 
   describe "touch_out" do
-    before{oystercard.top_up(OysterCard::MAX_BALANCE)}
-    before{oystercard.touch_in}
 
     it { is_expected.to respond_to(:touch_out) }
 
-    it "returns if card is in journey" do
-      oystercard.touch_out
-      expect(oystercard).not_to be_in_journey
-    end
+    context "oystercard at max-balance" do
+      before{oystercard.top_up(OysterCard::MAX_BALANCE)}
+      before{oystercard.touch_in(station)}
 
-  # In order to pay for my journey
-  # As a customer
-  # When my journey is complete, I need the correct amount deducted from my card
-    
-    it "charges the minimum fair" do
-      expect{ oystercard.touch_out }.to change{ oystercard.balance }.by -(OysterCard::MIN_TOUCH_IN_BALANCE)
+      it "returns if card is in journey" do
+        oystercard.touch_out
+        expect(oystercard).not_to be_in_journey
+      end
+
+      it "returns entry_station to nil" do # Test Possibly not needed? 
+        oystercard.touch_out
+        expect(oystercard.entry_station).to eq (nil)
+      end
+      # In order to pay for my journey
+      # As a customer
+      # When my journey is complete, I need the correct amount deducted from my card
+      it "charges the minimum fair" do
+        expect{ oystercard.touch_out }.to change{ oystercard.balance }.by -(OysterCard::MIN_TOUCH_IN_BALANCE)
+      end
     end
   end
 end
